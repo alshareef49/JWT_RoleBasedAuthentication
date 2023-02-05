b6f584ab00b578fde9096fe86e40ff0271c20221
alshareef49 <alshareef4039@gmail.com>
2023-02-05 23:35:20 +0530

    created gitController,jwtRequest and jwtResponse

---
+++ jwt_backend/src/main/java/com/alshareef/jwt_backend/controller/JwtController.java
@@ -0,0 +1,23 @@
+package com.alshareef.jwt_backend.controller;
+
+import com.alshareef.jwt_backend.entity.JwtRequest;
+import com.alshareef.jwt_backend.entity.JwtResponse;
+import com.alshareef.jwt_backend.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping({"/authenticate"})
+    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception{
+        return jwtService.createJwtToken(jwtRequest);
+    }
+}
--- jwt_backend/src/main/java/com/alshareef/jwt_backend/util/JwtUtil.java
+++ jwt_backend/src/main/java/com/alshareef/jwt_backend/util/JwtUtil.java
@@ -3,16 +3,21 @@ package com.alshareef.jwt_backend.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Component
 public class JwtUtil {
 
     private static final String SECRET_KEY = "SECRET_KEY_FOR_U";
+    private static final int TOKEN_VALIDITY = 3600*5;
 
     public String getUserNameFromToken(String token){
         return getClaimFromToken(token,Claims::getSubject);
@@ -39,5 +44,16 @@ public class JwtUtil {
 
     private Date getExpirationDateFromToken(String token){
         return getClaimFromToken(token,Claims::getExpiration);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+TOKEN_VALIDITY*1000))
+                .signWith(SignatureAlgorithm.HS512,SECRET_KEY)
+                .compact();
     }
 }
